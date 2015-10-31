88630ee373e9f5233bc389cb9d8c905f237f7925
wonyoung.ju <getogrand@paran.com>
2015-10-31 16:25:54 +0900

    recvmail for test

---
+++ mailrecv/mailrecv.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import smtpd
+import asyncore
+
+
+class CustomSMTPServer(smtpd.SMTPServer):
+
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        print('Receiving message from:', peer)
+        print('Message addressed from:', mailfrom)
+        print('Message addressed to  :', rcpttos)
+        print('Message length        :', len(data))
+        return
+
+server = CustomSMTPServer(('127.0.0.1', 1025), None)
+
+asyncore.loop()
