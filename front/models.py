e502666dbcc4e9829364fc3a39bf4b2da2482d89
kyunooh <chm073@gmail.com>
2015-11-13 01:48:29 +0900

    update invalid spell

--- front/models.py
+++ front/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Mail(models.Model):
-    recepient = models.CharField(max_length=50)
+    recipient = models.CharField(max_length=50)
     sender = models.CharField(max_length=200)
     subject = models.CharField(max_length=400)
     contents = models.TextField()
