fcf4153bbfd38c23f597a873e836bb373441b155
kyunooh <chm073@gmail.com>
2015-11-13 01:44:29 +0900

    add remove whitespace logic and refactor util

--- recvmail/util.py
+++ recvmail/util.py
@@ -3,10 +3,11 @@
 
 def nomalize_recip(recip):
     before_at_sign = recip[:recip.rfind('@')]
+    real = ''
     if before_at_sign.find('<') >= 0:
-        return before_at_sign[before_at_sign.find('<') + 1:]
+        real = before_at_sign[before_at_sign.find('<') + 1:]
     else:
-        return before_at_sign
-        
+        real = before_at_sign
+    return real.strip()
         
     
