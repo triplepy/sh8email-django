405a8f3796e4ab71ee631315ef5b12fb6897cca1
kyunooh <chm073@gmail.com>
2015-11-27 00:16:10 +0900

    add MultiRecvMailTest and pep8

--- recvmail/tests.py
+++ recvmail/tests.py
@@ -34,15 +34,15 @@ class RecvMailTest(TestCase):
                             self.msg.as_string())
         finally:
             server.quit()
-        # for wait to process mail    
+        # for wait to process mail
         time.sleep(0.1)
 
     def set_self_msg(self):
-        self.msg['To'] = email.utils.formataddr(('Recipient',
-                                            self.peer))
+        self.msg['To'] = email.utils.formataddr(
+            ('Recipient', self.peer))
         self.msg['From'] = email.utils.formataddr(('Author', self.frommail))
         self.msg['Subject'] = 'Simple test message'
-        
+
     def start_mail_server(self):
         p = Sh8MailProcessForTest()
         p.daemon = True
@@ -60,10 +60,54 @@ class RecvMailTest(TestCase):
         self.assertEquals(self.msg.get_payload(), mail.contents)
 
 
+class MultiRecvMailTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.msg = MIMEText('This is the body of the message.')
+        cls.frommail = 'author@example.com'
+        cls.peers = ['recipient@example.com',
+                     'recp2@example.com', 'recp3@example.com']
+        cls.peernickname = 'recipient'
+        super(MultiRecvMailTest, cls).setUpClass()
+        MultiRecvMailTest.start_mail_server(cls)
+        # for wait running server
+        time.sleep(0.1)
+        MultiRecvMailTest.send_test_mail(cls)
+
+    def send_test_mail(self):
+        self.set_self_msg(self)
+
+        server = smtplib.SMTP('127.0.0.1', 25)
+        # show communication with the server
+        try:
+            server.sendmail(self.frommail,
+                            self.peers,
+                            self.msg.as_string())
+        finally:
+            server.quit()
+        # for wait to process mail
+        time.sleep(0.1)
+
+    def set_self_msg(self):
+        self.msg['To'] = email.utils.formataddr(
+            ('Recipient', self.peers))
+        self.msg['From'] = email.utils.formataddr(('Author', self.frommail))
+        self.msg['Subject'] = 'Simple test message'
+
+    def test_get_mail_count(self):
+        mails = Mail.objects.all()
+        self.assertEquals(len(mails), len(self.peers))
+
+    def start_mail_server(self):
+        p = Sh8MailProcessForTest()
+        p.daemon = True
+        p.start()
+
+
 class MailUtil(TestCase):
     empty_mailfrom = ""
     empty_peer = ""
-    
+
     def assert_after_nomalize_recipent(self, param_email, expected):
         result = nomalize_recip(param_email)
         self.assertEquals(expected, result)
@@ -76,13 +120,13 @@ class MailUtil(TestCase):
 
     def test_nomalize_reciepent(self):
         self.assert_after_nomalize_recipent(
-                            "recipient@example.com", "recipient")
+            "recipient@example.com", "recipient")
         self.assert_after_nomalize_recipent(
-                            "recip@ent@example.com", "recip@ent")
+            "recip@ent@example.com", "recip@ent")
         self.assert_after_nomalize_recipent(
-                            "Recipient : <recipient@example.com>", "recipient")
+            "Recipient : <recipient@example.com>", "recipient")
         self.assert_after_nomalize_recipent(
-                            "Recipient : < recipient@example.com >", "recipient")
+            "Recipient : < recipient@example.com >", "recipient")
 
     def test_nomalize_body_case_with_only_body(self):
         p_body = self.make_default_parameter_body()
@@ -95,16 +139,13 @@ class MailUtil(TestCase):
     def test_nomalize_body_case_with_mailfrom(self):
         p_body = self.make_default_parameter_body()
         p_mailfrom = "mailfrom@example.com"
-        
+
         result_body = nomalize_body(p_body, p_mailfrom)
-        
+
         self.assertEquals("From <from@example.com>", result_body['From'])
         self.assertEquals("recipient", result_body['To'])
-        
-        
 
 
 class Sh8MailProcessForTest(Sh8MailProcess):
     def run(self):
         super(Sh8MailProcessForTest, self).run()
-
