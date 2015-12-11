fde66ddd87580a744b301dea67d6fa36939cee94
kyunooh <chm073@gmail.com>
2015-12-12 01:21:00 +0900

    commit for move to branch

--- front/models.py
+++ front/models.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta, MINYEAR
+from django.utils import timezone
 from django.db import models
 
 
@@ -7,10 +8,15 @@ class Mail(models.Model):
     sender = models.CharField(max_length=200)
     subject = models.CharField(max_length=400)
     contents = models.TextField()
-    recip_date = models.DateTimeField(auto_now_add=True)
+    recip_date = models.DateTimeField(auto_now_add=True, editable=True)
 
     def delete_one_day_ago(self):
-        yesterday = datetime.now() - timedelta(days=1)
-        mail_to_delete = self.objects.filter(recip_date__lte=(date))
+        yesterday = timezone.now() - timedelta(days=1)
+        mail_to_delete = self.objects.filter(
+            recip_date__lte=yesterday)
+        print(mail_to_delete)
+        mails = Mail.objects.all()
+        print("==============", mails[0].recip_date)
+        print("==============", mails[1].recip_date)
         mail_to_delete.delete()
 
--- front/tests.py
+++ front/tests.py
@@ -1,3 +1,36 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.test import TestCase
+from .models import Mail
 
-# Create your tests here.
+
+class BatchTest(TestCase):
+    def setUp(self):
+        # create mocking mail what was created before 24hour
+        yesterday = timezone.now() - timedelta(days=2)
+
+        Mail.objects.create(
+            recipient="test_delete_mail_util",
+            sender="delete_mail_sender",
+            subject="This will be deleted",
+            contents="코오오ㅇ온텐트",
+
+        )
+        Mail.objects.get(pk=1).update(recip_date=yesterday)
+        Mail.objects.create(
+            recipient="test_delete_mail_util",
+            sender="delete_mail_sender",
+            subject="This will be deleted",
+            contents="코오오ㅇ온텐트",
+        )
+
+
+    def test_delete_mail_util(self):
+        # call delete_mail_util method
+        Mail.delete_one_day_ago(Mail)
+        # check mail
+        mails = Mail.objects.all()
+        print("++++++|++++++", mails[0].recip_date)
+        print("++++++|++++++", mails[1].recip_date)
+        self.assertEquals(1, Mail.objects.count())
+
