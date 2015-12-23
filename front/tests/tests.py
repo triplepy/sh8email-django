2e0cbe3ac135347bd1f964d499e5d6b62935f992
wonyoung.ju <getogrand@paran.com>
2015-12-24 00:10:03 +0900

    wrote test of DetailView.

--- front/tests/tests.py
+++ front/tests/tests.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
-from django.test import TestCase
 from datetime import timedelta
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseForbidden
+from django.test import TestCase, Client
 from django.utils import timezone
 from front.checkin import MockCheckinManager
-
 from front.models import Mail
+from front.tests.utils import add_recip_to_session
 
 
 class MailTest(TestCase):
@@ -61,6 +64,45 @@ class MailTest(TestCase):
         return Mail.objects.create(recipient=recipient, sender=sender, subject=subject,
                                    contents=contents, is_read=is_read)
 
+
 class DetailViewTest(TestCase):
-    # TODO should make tests.
-    pass
+    def setUp(self):
+        self.recipient = 'ggone'
+        self.client = Client()
+        self.mail = Mail.objects.create(recipient=self.recipient, sender='sender1',
+                                        subject='subject1', contents='contents1')
+
+    def test_simplest_success_case(self):
+        add_recip_to_session(self.client, self.recipient)
+        self.response = self.client.get(
+                reverse('front:detail', args=(self.mail.pk,)))
+
+        self.assertContains(self.response, self.mail.subject)
+
+    def test_not_checkin(self):
+        self.response = self.client.get(
+                reverse('front:detail', args=(self.mail.pk,)))
+        self.assertEqual(self.response.status_code,
+                         HttpResponseForbidden.status_code)
+
+    def test_checkin_other_recipient(self):
+        checkin_recipient = 'wonyoung'
+        add_recip_to_session(self.client, checkin_recipient)
+
+        self.response = self.client.get(
+                reverse('front:detail', args=(self.mail.pk,)))
+
+        self.assertEqual(self.response.status_code,
+                         HttpResponseForbidden.status_code)
+
+    def test_not_exists_mail(self):
+        notexistsmail_pk = self.mail.pk
+        # After now, the mail isn't exists.
+        self.mail.delete()
+
+        add_recip_to_session(self.client, self.recipient)
+
+        self.response = self.client.get(
+                reverse('front:detail', args=(notexistsmail_pk,)))
+
+        self.assertEqual(self.response.status_code, 404)
