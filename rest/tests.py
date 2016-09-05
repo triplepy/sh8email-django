2e4a0149629911f4faeea4c11786662ba5b16336
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-09-05 23:00:55 +0900

    add rest test code

--- rest/tests.py
+++ rest/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import requests
+import time
 
-# Create your tests here.
+from django.test import LiveServerTestCase
+
+from sh8core.models import Mail
+
+
+class MailReceive(LiveServerTestCase):
+    def test_mail_received(self):
+        mail_data = {
+            "recipient": "test_mail_received",
+            "sender": "sender@sh8.email",
+            "subject": "test_mail_received subject",
+            "contents": "contconctonctonctonctoncton$$$$$$$$!@#@!%!^#$%^$%^&^&%(("
+        }
+
+        requests.post(url=self.live_server_url + "/rest/mail/", data=mail_data)
+        mail = Mail.objects.get(recipient=mail_data["recipient"])
+
+        self.assertTrue(mail)
+        self.assertEqual(mail.sender, mail_data["sender"])
+        self.assertEqual(mail.subject, mail_data["subject"])
+        self.assertEqual(mail.contents, mail_data["contents"])
