e60fa654568084ecd2c127be9486d01426533207
ChoiHyeonMook <chm073@gmail.com>
2016-09-11 18:15:52 +0900

    remove mailreceive rest and add pk on serializers

--- sh8core/serializers.py
+++ sh8core/serializers.py
@@ -3,6 +3,7 @@ from sh8core.models import Mail
 
 
 class MailSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
     recipient = serializers.CharField(max_length=50)
     secret_code = serializers.CharField(max_length=16)
     sender = serializers.CharField(max_length=200)
@@ -10,6 +11,3 @@ class MailSerializer(serializers.Serializer):
     contents = serializers.CharField()
     recip_date = serializers.DateTimeField()
     is_read = serializers.BooleanField(default=False)
-
-
-
