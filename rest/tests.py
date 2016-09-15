97ed4901113d3a01ab2654f79ca12a9cd70c2cfd
Wonyoung Ju <getogrand@gmail.com>
2016-09-16 00:29:13 +0900

    Add test of api that retrieve mail list.

--- rest/tests.py
+++ rest/tests.py
@@ -17,7 +17,8 @@ class RestAPITest(APITestCase):
         # when
         response = self.client.get(reverse('rest:rest-mail-detail', args=[mail.recipient, mail.pk]))
         # then
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         "response.content was {}".format(response.content.decode()))
         self.assertEqual(response.data['pk'], mail.pk)
         self.assertEqual(response.data['recipient'], mail.recipient)
         self.assertEqual(response.data['sender'], mail.sender)
@@ -26,7 +27,29 @@ class RestAPITest(APITestCase):
         self.assertDatetimeEqual(response.data['recip_date'], mail.recip_date)
         # is_read should be True after the api response (the 'api response' means that someone read the mail.)
         self.assertEqual(response.data['is_read'], True)
-        self.assertEqual(response.data['isSecret'], mail.is_secret)
+
+    def test_retrieve_mail_list(self):
+        # given
+        recipient = 'silver'
+        mails = Mail.objects.filter(recipient=recipient)
+        # add_recip_to_session(self.client, recipient)
+        # when
+        response = self.client.post(reverse('rest:rest-mail-list', args=[recipient]), data={'recipient': recipient})
+        # then
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         "response.content was {}".format(response.content.decode()))
+        for index, d in enumerate(response.data):
+            self.assertEqual(d['pk'], mails[index].pk)
+            self.assertEqual(d['recipient'], mails[index].recipient)
+            self.assertEqual(d['sender'], mails[index].sender)
+            self.assertEqual(d['subject'], mails[index].subject)
+            if mails[index].is_secret():
+                self.assertEqual(d['contents'], '')
+            else:
+                self.assertEqual(d['contents'], mails[index].contents)
+            self.assertDatetimeEqual(d['recip_date'], mails[index].recip_date)
+            self.assertEqual(d['isSecret'], mails[index].is_secret())
+            self.assertEqual(d['is_read'], mails[index].is_read)
 
     # TODO move to mixin.
     def assertDatetimeEqual(self, datetime_str, datetime_obj):
