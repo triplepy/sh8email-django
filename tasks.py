a386243d0a211ed764d7e7aa7a8a0e945858d367
Wonyoung Ju <getogrand@gmail.com>
2016-09-17 23:19:20 +0900

    Add banner printing to deploy task.

--- tasks.py
+++ tasks.py
@@ -4,11 +4,19 @@ from invoke import task
 from invoke_tasks import test
 
 
+def print_banner(command):
+    print("============= RUN {} ===============".format(command))
+
+
 @task
 def deploy(ctx):
+    print_banner("git pull")
     ctx.run("git pull")
+    print_banner("pip install -r requirements.txt")
     ctx.run("pip install -r requirements.txt")
+    print_banner("python manage.py collectstatic --noinput")
     ctx.run("python manage.py collectstatic --noinput")
+    print_banner("touch ../reload")
     ctx.run("touch ../reload")
 
 
