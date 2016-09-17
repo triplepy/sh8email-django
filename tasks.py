6082497cefbaad149510648ebdde8c2b1684450f
Wonyoung Ju <getogrand@gmail.com>
2016-09-17 23:08:46 +0900

    Add deploy task.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,14 @@
 from invoke import Collection
+from invoke import task
 
 from invoke_tasks import test
 
 
-namespace = Collection(test)
+@task
+def deploy(ctx):
+    ctx.run("git pull")
+    ctx.run("python manage.py collectstatic --noinput")
+    ctx.run("touch ../reload")
+
+
+namespace = Collection(deploy, test)
