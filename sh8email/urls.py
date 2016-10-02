92df2b21b0f2d631f0a77b146aff5a0f1acd00f0
ChoiHyeonMook <chm073@gmail.com>
2016-10-02 22:06:22 +0900

    add create dummies feature

--- sh8email/urls.py
+++ sh8email/urls.py
@@ -19,10 +19,13 @@ from django.contrib import admin
 from rest import urls as rest_urls
 from web import urls as web_urls
 from web import views as web_views
+from sh8core import views as core_views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^checkin/$', web_views.checkin, name='checkin'),
     url(r'', include(web_urls, namespace='web')),
     url(r'^rest/', include(rest_urls, namespace='rest')),
+    url(r'^create-dummies/$', core_views.create_dummies, name='create_dummies')
 ]
