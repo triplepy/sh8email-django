39249ea0108d73c9867fd96371616618cb3a650f
Wonyoung Ju <getogrand@gmail.com>
2016-10-08 20:58:35 +0900

    Print message via print() with timestamp.

--- abstractions/process_command.py
+++ abstractions/process_command.py
@@ -3,10 +3,10 @@ import signal
 
 from django.conf import settings
 from django.core.management import BaseCommand
+from django.utils import timezone
 
 
 class ProcessCommand(BaseCommand):
-
     def __init__(self):
         super().__init__()
         self.pid_file_dir = os.path.join(settings.BASE_DIR, 'tmp', 'pids')
@@ -24,7 +24,7 @@ class ProcessCommand(BaseCommand):
     def start_process(self):
         p = self.process_class()
         p.start()
-        print("{} IS STARTED".format(self.process_class.__name__))
+        self._print_with_timestamp("{} IS STARTED".format(self.process_class.__name__))
         self.store_pid(p)
 
     def store_pid(self, p):
@@ -38,11 +38,11 @@ class ProcessCommand(BaseCommand):
 
     def stop_process(self):
         if not os.path.isfile(self.pid_file_path):
-            print("There is no running process to stop.")
+            self._print_with_timestamp("There is no running process to stop.")
             return
 
         os.kill(self._read_pid(), signal.SIGTERM)
-        print("{} IS STOPPED".format(self.process_class.__name__))
+        self._print_with_timestamp("{} IS STOPPED".format(self.process_class.__name__))
 
         os.remove(self.pid_file_path)
 
@@ -50,3 +50,6 @@ class ProcessCommand(BaseCommand):
         with open(self.pid_file_path) as f:
             pid = f.read()
         return int(pid)
+
+    def _print_with_timestamp(self, msg):
+        print("[{}] {}".format(timezone.now().isoformat(), msg))
